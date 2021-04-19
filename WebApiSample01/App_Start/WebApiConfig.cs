aa46390e126dda16808fe9420e1cc5b5b06c28d4
taka1127 <taka07251127@gmail.com>
2021-04-19 21:51:50 +0900

    コンテントネゴシエーション、JSON

--- WebApiSample01/App_Start/WebApiConfig.cs
+++ WebApiSample01/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace WebApiSample01
@@ -10,8 +11,7 @@ namespace WebApiSample01
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-            //config.Formatters.Remove(config.Formatters.XmlFormatter);
-            //config.Formatters.Add(config.Formatters.JsonFormatter);
+           
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -21,6 +21,9 @@ namespace WebApiSample01
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
         }
     }
 }
